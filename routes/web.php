be1121f7e8c6b9669d35407f285e61a0056fe082
benlaktibyassine <yassinebenlaktib@gmail.com>
2023-12-20 21:19:45 +0100

    responsable

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('locales', 'LocalController');
+Route::resource('salles', 'SalleController');
+Route::resource('users', 'UserController');
+
