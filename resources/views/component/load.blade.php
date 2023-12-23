9c619b1d4efcf30b13863440bfb9c7e0becddf17
benlaktibyassine <yassinebenlaktib@gmail.com>
2023-12-23 18:11:33 +0100

    modifiaction creat prof

---
+++ resources/views/component/load.blade.php
@@ -0,0 +1,10 @@
+ <!-- Start Preloader Area -->
+ <div class="preloader">
+    <div class="lds-ripple">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</div>
+<!-- End Preloader Area  -->
